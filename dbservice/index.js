012cbc1cd4a250c2fd91eff219983756942fe6fa
Felix-Jacob <jacobfelix51@gmail.com>
2022-04-08 15:36:03 +0200

    fixed inter container communication

--- dbservice/index.js
+++ dbservice/index.js
@@ -8,7 +8,7 @@ app.listen(port, () => {
 })
 
 app.get('/', (req, res) => {
-   fetch('http://emilia:1234@localhost:5984/')
+   fetch('http://emilia:1234@172.17.0.2:5984/')
       .then(response => response.json())
       .then(data => res.send(data));
 })
